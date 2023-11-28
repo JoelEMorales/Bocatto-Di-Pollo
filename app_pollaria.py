9d3309cfa2849859fe16df2b1e82c39d82df26a1
Joel Morales <joelelianmorales@gmail.com>
2023-11-28 11:23:56 +0100

    modificaciones y actualizacion de codivo borrado anteriormente de git

--- app_pollaria.py
+++ app_pollaria.py
@@ -3,37 +3,20 @@ from bson import ObjectId
 from pymongo import MongoClient
 import os
 
+
 app = Flask(__name__, static_folder='static')
 
 client = MongoClient("mongodb://localhost:27017")
 basedatos = client.paginas
 misencargos = basedatos.lista
 
-# Rutas hacia las paginas
 
 
+
+# Rutas hacia la pagina
 @app.route("/")
 def pag_principal():
     return send_from_directory(app.root_path, 'index.html')
-
-
-@app.route("/our-product")
-def pag_our_product():
-    return render_template('product.html')
-
-
-@app.route("/product")
-def pag_producto():
-    return render_template('pagina_compra.html')
-
-
-
-@app.route('/resumen_compra')
-def pag_carrito():
-    # Puedes hacer aquí cualquier proceso necesario antes de renderizar la página su_carrito.html
-    return render_template('resumen_compra.html')
-    
-
 
 # Redirigir a galeria
 @app.route("/principal_galeria")
@@ -43,7 +26,6 @@ def pag_principal_galeria():
 @app.route("/galeria")
 def galeria():
     return send_from_directory(app.root_path, 'index.html')
-
 
 
 # Redirigir a contacto
@@ -56,21 +38,6 @@ def contacto():
     return send_from_directory(app.root_path, 'index.html')
 
 
-
-
-
-
-
-
-
-# Ruta para la página de contacto (donde procesarías el formulario)
-
-
-@app.route('/contact', methods=['POST'])
-def contact():
-    # Procesa el formulario y realiza las operaciones necesarias
-    # Aquí puedes acceder a los datos del formulario con request.form
-    return render_template('contact_response.html')
 
 
 if __name__ == "__main__":
