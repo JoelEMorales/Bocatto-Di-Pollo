bcbf5d567e6c07e0566e17817d24b5cde97d5b10
Joel Morales <joelelianmorales@gmail.com>
2023-11-29 00:01:19 +0100

    creacion de .env y variables de entorno

--- static/client/index.js
+++ static/client/index.js
@@ -1,100 +1,102 @@
-require('dotenv').config();
+require('dotenv').config({ path: '/home/joelmorles/Documents/PROYECTO-GITHUB/Bocatto-Di-Pollo/.env' });
+
 
 
 // Add SDK credentials
 // REPLACE WITH YOUR PUBLIC KEY AVAILABLE IN: https://developers.mercadopago.com/panel
 const mercadopago = new MercadoPago(process.env.MP_PUBLIC_KEY, {
     locale: 'es-AR' // The most common are: 'pt-BR', 'es-AR' and 'en-US'
-    });
-    
-    // Recupera el carrito del LocalStorage
-    var carrito = JSON.parse(localStorage.getItem("carrito")) || [];
-    
-    // Objeto para rastrear la cantidad de cada producto
-    const productosAgrupados = {};
-    
-    // Variable para sumar el precio total
-    let precioTotal = 0;
-    
-    // Agrupa los productos y suma sus cantidades
-    carrito.forEach(function (producto) {
+});
+
+
+// Recupera el carrito del LocalStorage
+var carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+
+// Objeto para rastrear la cantidad de cada producto
+const productosAgrupados = {};
+
+// Variable para sumar el precio total
+let precioTotal = 0;
+
+// Agrupa los productos y suma sus cantidades
+carrito.forEach(function (producto) {
     const clave = producto.nombre + (producto.aclaracion || ""); // Usamos una clave única basada en el nombre y aclaración (si existe)
     if (!productosAgrupados[clave]) {
-    productosAgrupados[clave] = {
-    nombre: producto.nombre,
-    cantidad: producto.cantidad,
-    aclaracion: producto.aclaracion,
-    imagen: producto.imagen,
-    valor: producto.total,
-    };
+        productosAgrupados[clave] = {
+            nombre: producto.nombre,
+            cantidad: producto.cantidad,
+            aclaracion: producto.aclaracion,
+            imagen: producto.imagen,
+            valor: producto.total,
+        };
     } else {
-    productosAgrupados[clave].cantidad += producto.cantidad;
+        productosAgrupados[clave].cantidad += producto.cantidad;
     }
-    
+
     // Suma el precio total
     precioTotal += parseFloat(producto.total);
-    });
-    
-    // Obtener el botón de pago fuera del bucle
-    const checkoutButton = document.getElementById("checkout-btn");
-    
-    // Handle call to backend and generate preference.
-    checkoutButton.addEventListener("click", function () {
-    
+});
+
+// Obtener el botón de pago fuera del bucle
+const checkoutButton = document.getElementById("checkout-btn");
+
+// Handle call to backend and generate preference.
+checkoutButton.addEventListener("click", function () {
+
     console.log("Botón de pago clickeado");
     checkoutButton.remove();
-    
+
     const orderData = {
-    quantity: 1,
-    description: "Total a pagar",
-    price: precioTotal,
+        quantity: 1,
+        description: "Total a pagar",
+        price: precioTotal,
     };
-    
+
     console.log("Datos del pedido:", orderData);
-    
+
     fetch("http://localhost:5000/create_preference", {
-    method: "POST",
-    headers: {
-    "Content-Type": "application/json",
-    },
-    body: JSON.stringify(orderData),
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(orderData),
     })
-    .then(function (response) {
-    return response.json();
-    })
-    .then(function (preference) {
-    console.log("Preferencia creada:", preference);
-    createCheckoutButton(preference.id);
-    })
-    .catch(function (error) {
-    console.error("Error en la solicitud:", error);
-    alert("Unexpected error");
-    checkoutButton.disabled = false;
-    });
-    });
-    
-    function createCheckoutButton(preferenceId) {
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (preference) {
+            console.log("Preferencia creada:", preference);
+            createCheckoutButton(preference.id);
+        })
+        .catch(function (error) {
+            console.error("Error en la solicitud:", error);
+            alert("Unexpected error");
+            checkoutButton.disabled = false;
+        });
+});
+
+function createCheckoutButton(preferenceId) {
     // Initialize the checkout
     const bricksBuilder = mercadopago.bricks();
-    
+
     const renderComponent = async (bricksBuilder) => {
-    console.log("Inicializando MercadoPago Bricks...");
-    
-    // if (window.checkoutButton) window.checkoutButton.unmount();
-    
-    await bricksBuilder.create(
-    "wallet",
-    "button-checkout", // class/id where the payment button will be displayed
-    {
-    initialization: {
-    preferenceId: preferenceId,
-    },
-    callbacks: {
-    onError: (error) => console.error(error),
-    onReady: () => { },
-    },
-    }
-    );
+        console.log("Inicializando MercadoPago Bricks...");
+
+        // if (window.checkoutButton) window.checkoutButton.unmount();
+
+        await bricksBuilder.create(
+            "wallet",
+            "button-checkout", // class/id where the payment button will be displayed
+            {
+                initialization: {
+                    preferenceId: preferenceId,
+                },
+                callbacks: {
+                    onError: (error) => console.error(error),
+                    onReady: () => { },
+                },
+            }
+        );
     };
     window.checkoutButton = renderComponent(bricksBuilder);
-    }
+}
--- static/server/server.js
+++ static/server/server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const mercadopago = require("mercadopago");
-const config = require('../../config/config.js');
-require('dotenv').config();
+
+require('dotenv').config({ path: '/home/joelmorles/Documents/PROYECTO-GITHUB/Bocatto-Di-Pollo/.env' });
 
 
 
 mercadopago.configure({
-  access_token: process.env.MP_ACCESS_TOKEN,
+  access_token: process.env.MP_ACCESS_TOKEN
 });
 
 
@@ -19,45 +19,45 @@ app.use('/static', express.static("/home/joelmorles/Documents/PROYECTO-GITHUB/Bo
 app.use(express.static("/home/joelmorles/Documents/PROYECTO-GITHUB/Bocatto-Di-Pollo/static"));
 app.use(cors());
 app.get("/", function (req, res) {
-res.status(200).sendFile("/home/joelmorles/Documents/PROYECTO-GITHUB/Bocatto-Di-Pollo/index.html");
+  res.status(200).sendFile("/home/joelmorles/Documents/PROYECTO-GITHUB/Bocatto-Di-Pollo/index.html");
 });
 
 app.post("/create_preference", (req, res) => {
 
-let preference = {
-items: [
-{
-title: req.body.description,
-unit_price: Number(req.body.price),
-quantity: Number(req.body.quantity),
-}
-],
-back_urls: {
-"success": "http://localhost:5000",
-"failure": "http://localhost:5000",
-"pending": "http://localhost:5000"
-},
-auto_return: "approved",
-};
+  let preference = {
+    items: [
+      {
+        title: req.body.description,
+        unit_price: Number(req.body.price),
+        quantity: Number(req.body.quantity),
+      }
+    ],
+    back_urls: {
+      "success": "http://localhost:5000",
+      "failure": "http://localhost:5000",
+      "pending": "http://localhost:5000"
+    },
+    auto_return: "approved",
+  };
 
-mercadopago.preferences.create(preference)
-.then(function (response) {
-res.json({
-id: response.body.id
-});
-}).catch(function (error) {
-console.log(error);
-});
+  mercadopago.preferences.create(preference)
+    .then(function (response) {
+      res.json({
+        id: response.body.id
+      });
+    }).catch(function (error) {
+      console.log(error);
+    });
 });
 
 app.get('/feedback', function (req, res) {
-res.json({
-Payment: req.query.payment_id,
-Status: req.query.status,
-MerchantOrder: req.query.merchant_order_id
-});
+  res.json({
+    Payment: req.query.payment_id,
+    Status: req.query.status,
+    MerchantOrder: req.query.merchant_order_id
+  });
 });
 
 app.listen(5000, () => {
-console.log("The server is now running on Port 5000");
+  console.log("The server is now running on Port 5000");
 });
